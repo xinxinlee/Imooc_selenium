4c6e9c3dddf89bc6f5886c730dc61fcea8504900
xinxinlee <67929057@qq.com>
2020-09-07 18:37:17 +0800

    unittest执行case的多种方式

--- case/unittest_case.py
+++ case/unittest_case.py
@@ -1,12 +1,12 @@
 import unittest
 
 class FirstCase01(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
+    def setUp(self):
         print('前置')
-    @classmethod
-    def tearDownClass(cls):
+
+    def tearDown(self):
         print('后置')
+
     def test_first01(self):
         print("test01")
 
@@ -14,4 +14,9 @@ class FirstCase01(unittest.TestCase):
         print('test02')
 
 if __name__ == '__main__':
-    unittest.main()
+    #case_path = 'D:\Imooc_selenium\case'
+    #suite = unittest.defaultTestLoader.discover(case_path, pattern='unittest*.py', top_level_dir=None)
+    #unittest.TextTestRunner().run(suite)
+    suite = unittest.TestSuite()
+    suite.addTest(FirstCase01("test_first02"))
+    unittest.TextTestRunner().run(suite)
