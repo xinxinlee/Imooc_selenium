66e2f20f505918eaeaf6c3ee4697239b7af59bfa
xinxinlee <67929057@qq.com>
2020-11-10 15:37:44 +0800

    添加读取配置文件编码参数

--- util/read_ini.py
+++ util/read_ini.py
@@ -8,16 +8,18 @@ class ReadIni:
 
     def load_ini(self,file_name):
         cf = configparser.ConfigParser()
-        cf.read(file_name)
+        cf.read(file_name,encoding='utf-8')
         return cf
+
     def get_value(self,element_name):
         element = self.cf.get(self.node,element_name)
         return element
 
 if __name__ == '__main__':
-    file_name = ''
-    read_obj = ReadIni('D:\\Imooc_selenium\\config\\LocalElement.ini','RegisterElement')
+    file_name = 'D:\\Imooc_selenium\\config\\LocalElement.ini'
+    read_obj = ReadIni(file_name,'RegisterElement')
     a = read_obj.get_value('shop_name')
     print(a)
+    print(type(a))
 
 
